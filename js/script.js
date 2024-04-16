6610e5c595f0d4e3942d5fb37e47051970dbf15d
Jay <j.adeyina@gmail.com>
2024-04-16 12:38:09 +0900

    Improved regex

--- js/script.js
+++ js/script.js
@@ -10,8 +10,9 @@ notifyButton.addEventListener("click", function () {
 
 //Validation function 
 const validateEmail = function () {
-    const regularExpression = /^([a-z]+)@([a-zA-Z0-9\._]+)\.([a-z]+)([\.]?)/;
-    
+    // const regularExpression = /^([a-z]+)@([a-zA-Z0-9\._]+)\.([a-z]+)([\.]?)/;
+    const regularExpression = /^([a-z\d\.-]+)@([a-z\d]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/
+
     //else if match regular expression, return true. 
     if (!emailInput.value.match(regularExpression)) {
         message.innerHTML = `<p>Please provide a valid email address.</p>`;
